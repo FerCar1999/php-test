bfb83e723881543b8cd8cc4f2b78f7f637bb31f4
Fernando Carranza <foreverfas_fc@hotmail.com>
2021-12-10 15:29:36 -0600

    fist commit

---
+++ database/migrations/2021_12_10_212806_create_grd_grado_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGrdGradoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grd_grado', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grd_grado');
+    }
+}
